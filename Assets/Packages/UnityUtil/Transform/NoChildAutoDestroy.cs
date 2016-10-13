3a9635911f83c9b61cf816f09c19e0d842565a94
fukunaga <fukunaga@team-lab.com>
2016-10-13 18:58:06 +0900

    NoChidlAutoDestory - add waitChildGenerate flag - add test scene

--- Assets/Packages/UnityUtil/Transform/NoChildAutoDestroy.cs
+++ Assets/Packages/UnityUtil/Transform/NoChildAutoDestroy.cs
@@ -3,7 +3,18 @@ using System.Collections;
 
 public class NoChildAutoDestroy : MonoBehaviour {
 
+    // 一度子供ができるまで待つ
+    public bool waitChildGenerate;
+
+    bool isChildGenerated;
+
 	void Update () {
-		if ( transform.childCount <= 0 ) Destroy(gameObject);
+        var hasChild = transform.childCount > 0;
+        isChildGenerated |= hasChild;
+
+        if (!hasChild && (!waitChildGenerate || isChildGenerated))
+        {
+            Destroy(gameObject);
+        }
 	}
 }
