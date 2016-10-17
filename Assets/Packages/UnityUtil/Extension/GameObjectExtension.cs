800b7f459ef66da7351aa7a8323787f932440577
fukunaga <fukunaga@team-lab.com>
2016-10-17 20:29:42 +0900

    add GameObjectExtension

---
+++ Assets/Packages/UnityUtil/Extension/GameObjectExtension.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class GameObjectExtension
+{
+    static public T GetOrAddComponent<T>(this GameObject go) where T : Component
+    {
+        return go.GetComponent<T>() ?? go.AddComponent<T>();
+    }
+}
