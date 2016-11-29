255396c0f497ca079c8718bf411e9ff54a29d404
fukunaga <fukunaga@team-lab.com>
2016-11-29 20:50:21 +0900

    add VectorExtension

---
+++ Assets/Packages/UnityUtil/Extension/VectorExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+static public class VectorExtension
+{
+    static public Vector3 Abs(this Vector3 v)
+    {
+        return new Vector3(
+            Mathf.Abs(v.x),
+            Mathf.Abs(v.y),
+            Mathf.Abs(v.z)
+           );
+    }
+}
