93cf009db1d443b151937fee440fac0d299170a0
fukunaga <fukunaga@team-lab.com>
2017-10-05 17:26:25 +0900

    add ComputeShaderExtension

---
+++ Assets/Packages/UnityUtil/Extension/ComputeShaderExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityUtil
+{
+    public static class ComputeShaderExtension
+    {
+        public static void DispatchThreads(this ComputeShader cs, int kernelIndex, int threadNumX, int threadNumY, int threadNumZ)
+        {
+            uint x, y, z;
+            cs.GetKernelThreadGroupSizes(kernelIndex, out x, out y, out z);
+
+            cs.Dispatch(kernelIndex, Mathf.CeilToInt((float)threadNumX / x), Mathf.CeilToInt((float)threadNumY / y), Mathf.CeilToInt((float)threadNumZ / z));
+        }
+    }
+
+}
