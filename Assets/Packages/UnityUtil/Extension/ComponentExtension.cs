29e4dd6fb280cad996db794c443f86b29b2e9cc2
fuqunaga <fukhid@gmail.com>
2018-12-06 17:54:05 +0900

    upgrade unity2018.2.13f1 remove MonoBehaviourExtension

---
+++ Assets/Packages/UnityUtil/Extension/ComponentExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UnityUtil
+{
+    static public class ComponentExtension
+    {
+        static public List<T> GetComponentsAll<T>(this Component c) where T : Component
+        {
+            return c.transform.GetComponentsAll<T>();
+
+        }
+    }
+}
--- Assets/Packages/UnityUtil/Extension/IntExtension.cs
+++ Assets/Packages/UnityUtil/Extension/IntExtension.cs
@@ -3,7 +3,7 @@
 
     static public class IntExtension
     {
-        static public void times(this int a, System.Action action)
+        static public void Times(this int a, System.Action action)
         {
             for (var i = 0; i < a; ++i)
             {
@@ -11,13 +11,12 @@
             }
         }
 
-        static public void times(this int a, System.Action<int> action)
+        static public void Times(this int a, System.Action<int> action)
         {
             for (var i = 0; i < a; ++i)
             {
                 action(i);
             }
         }
-
     }
 }
