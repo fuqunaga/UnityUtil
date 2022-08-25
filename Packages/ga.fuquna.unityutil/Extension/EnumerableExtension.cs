364a674cdaf4c2711dbf206a691e1c6cffa65f75
fuqunaga <fukhid@gmail.com>
2022-08-25 16:00:09 +0900

    add EnumerableExtensions.MaxBy()

--- Packages/ga.fuquna.unityutil/Extension/EnumerableExtension.cs
+++ Packages/ga.fuquna.unityutil/Extension/EnumerableExtension.cs
@@ -74,5 +74,32 @@ namespace UnityUtil
             }
             return min;
         }
+        
+        public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector, IComparer<TKey> comparer = null)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (selector == null) throw new ArgumentNullException(nameof(selector));
+            comparer ??= Comparer<TKey>.Default;
+
+            using var sourceIterator = source.GetEnumerator();
+            
+            if (!sourceIterator.MoveNext())
+            {
+                throw new InvalidOperationException("Sequence contains no elements");
+            }
+            var max = sourceIterator.Current;
+            var maxKey = selector(max);
+            while (sourceIterator.MoveNext())
+            {
+                var candidate = sourceIterator.Current;
+                var candidateProjected = selector(candidate);
+                if (comparer.Compare(candidateProjected, maxKey) > 0)
+                {
+                    max = candidate;
+                    maxKey = candidateProjected;
+                }
+            }
+            return max;
+        }
     }
 }
