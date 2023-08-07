1d5b5b92a0568697e20eedd96130729a0ed56c38
fuqunaga <fukhid@gmail.com>
2023-08-07 19:05:54 +0900

    add LineSegment.hlsl , LineSegmentGpuTest

---
+++ Assets/Example/Geometry/LineSegmentGpuTest.cs
@@ -0,0 +1,112 @@
+﻿using System.Runtime.InteropServices;
+using Unity.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UnityUtil.Example
+{
+    // [ExecuteAlways]
+    public class LineSegmentGpuTest : MonoBehaviour
+    {
+        private static class CsParam
+        {
+            public static readonly int Point = Shader.PropertyToID("_Point");
+            public static readonly int LineSegmentBuffer = Shader.PropertyToID("_LineSegmentBuffer");
+            public static readonly int OutputBuffer = Shader.PropertyToID("_OutputBuffer");
+        }
+
+        private struct Output
+        {
+            public int hasHit;
+            public Vector2 hitPoint;
+            public Vector2 closestPoint0;
+            public Vector2 closestPoint1;
+            public float distance0;
+            public float distance1;
+            public int isLeft0;
+            public int isLeft1;
+        }
+        
+        
+        [FormerlySerializedAs("_computeShader")] public ComputeShader computeShader;
+        
+        [FormerlySerializedAs("line0_start")] public Transform line0Start;
+        [FormerlySerializedAs("line0_end")] public Transform line0End;
+        [FormerlySerializedAs("line1_start")] public Transform line1Start;
+        [FormerlySerializedAs("line1_end")] public Transform line1End;
+
+        [FormerlySerializedAs("point")] public Transform targetPoint;
+
+        private int _kernelIndex = -1;
+        private GraphicsBuffer _lineSegmentBuffer;
+        private GraphicsBuffer _outputBuffer;
+        
+
+        private void OnDrawGizmos()
+        {
+            var point = targetPoint.position;
+
+            var line0 = new LineSegment() { start = line0Start.position, end = line0End.position };
+            var line1 = new LineSegment() { start = line1Start.position, end = line1End.position };
+
+            DispatchComputeShader(point, line0, line1);
+            
+            var outputArray = new Output[1];
+            _outputBuffer.GetData(outputArray);
+            var output = outputArray[0];
+            
+
+            // line0
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(line0.start, line0.end);
+
+            // line1
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(line1.start, line1.end);
+
+            // intersection line0,line1
+            if (output.hasHit > 0)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(output.hitPoint, 0.3f);
+            }
+
+
+            // closet point
+            Gizmos.color = Color.gray;
+            Gizmos.DrawLine(output.closestPoint0, point);
+            Gizmos.DrawLine(output.closestPoint1, point);
+
+
+#if UNITY_EDITOR
+            Handles.Label(point, $"Distance: {output.distance0},{output.distance1}");
+            Handles.Label(point - Vector3.up* 0.2f, $"IsLeft: {output.isLeft0},{output.isLeft1}");
+#endif
+        }
+
+        private void DispatchComputeShader(Vector2 point, LineSegment line0, LineSegment line1)
+        {
+            if (_kernelIndex < 0)
+            {
+                _kernelIndex = computeShader.FindKernel("CSMain");
+            }
+
+            _lineSegmentBuffer ??= new GraphicsBuffer(GraphicsBuffer.Target.Structured, 2, Marshal.SizeOf<LineSegment>());
+            _outputBuffer ??= new GraphicsBuffer(GraphicsBuffer.Target.Structured, 1, Marshal.SizeOf<Output>());
+
+            
+            var array = new NativeArray<LineSegment>(2, Allocator.Temp);
+            array[0] = line0;
+            array[1] = line1;
+            _lineSegmentBuffer.SetData(array);
+            array.Dispose();
+            
+            computeShader.SetFloats(CsParam.Point, point.x, point.y);
+            computeShader.SetBuffer(_kernelIndex, CsParam.LineSegmentBuffer, _lineSegmentBuffer);
+            computeShader.SetBuffer(_kernelIndex, CsParam.OutputBuffer, _outputBuffer);
+            
+            computeShader.Dispatch(_kernelIndex, 1, 1, 1);
+        }
+    }
+}
---
+++ Packages/ga.fuquna.unityutil/Geometry/LineSegment.hlsl
@@ -0,0 +1,112 @@
+﻿#ifndef LINE_SEGMENT_HLSL
+#define LINE_SEGMENT_HLSL
+
+struct LineSegment
+{
+	float2 start;
+	float2 end;
+};
+
+
+bool IsLeft(float2 pos, LineSegment ls)
+{
+	float2 start = ls.start;
+	float2 end = ls.end;
+
+	float2 toEnd = end - start;
+	float2 toPos = pos - start;
+
+	return cross(float3(toEnd,0), float3(toPos,0)).z > 0;
+}
+
+// 点との距離の二乗
+// https://zenn.dev/boiledorange73/articles/0037-js-distance-pt-seg
+float LineDistanceSq(float2 pos, LineSegment ls)
+{
+	float2 start = ls.start;
+	float2 end = ls.end;
+
+	float2 posToStart = start - pos;
+
+	float2 ab = end - start;
+	float r2 = dot(ab, ab);
+	float tt = -dot(ab, posToStart);
+	if (tt < 0)
+	{
+		return dot(posToStart, posToStart);
+	}
+	
+	if (tt > r2)
+	{
+		float2 posToEnd = end - pos;
+		return dot(posToEnd, posToEnd);
+	}
+	
+	float f1 = ab.x * posToStart.y - ab.y * posToStart.x;
+	return (f1 * f1) / r2;
+}
+
+float LineDistance(float2 pos, LineSegment ls)
+{
+	return sqrt(LineDistanceSq(pos, ls));
+}
+
+float LineClosestPointRate(float2 pos, LineSegment ls)
+{
+	float2 start = ls.start;
+	float2 end = ls.end;
+
+	float2 posToStart = start - pos;
+
+	float2 ab = end - start;
+	float r2 = dot(ab, ab);
+	float tt = -dot(ab, posToStart);
+
+	return saturate(tt/r2);
+}
+
+inline float2 CalcPointOnLineFromRate(LineSegment ls, float rateOnSegment)
+{
+	return lerp(ls.start, ls.end, rateOnSegment);
+}
+
+float2 LineClosestPoint(float2 pos, LineSegment ls)
+{
+	float rate = LineClosestPointRate(pos, ls);
+	return CalcPointOnLineFromRate(ls, rate);
+}
+
+float2 GetNormal(LineSegment ls)
+{
+	float2 dir = ls.end - ls.start;
+	float2 lineNormal = float2( -dir.y, dir.x); // rotate 90 degree
+	return normalize(lineNormal);
+}
+
+
+//交点を求める
+//https://www.hiramine.com/programming/graphics/2d_segmentintersection.html
+//https://gist.github.com/yoshiki/7702066
+bool CalcIntersection(LineSegment lhs, LineSegment rhs, out float2 hitPosition)
+{
+	hitPosition = (0).xx;
+	
+	float2 a = lhs.start;
+	float2 b = lhs.end;
+	float2 c = rhs.start;
+	float2 d = rhs.end;
+
+	float div = (b.x - a.x) * (d.y - c.y) - (b.y - a.y) * (d.x - c.x);
+
+	float2 ac = c - a;
+	float u = ((d.y - c.y) * ac.x - (d.x - c.x) * ac.y) / div;
+	float s = ((b.y - a.y) * ac.x - (b.x - a.x) * ac.y) / div;
+
+	if (!isfinite(u) || u < 0.0 || 1.0 < u) return false;
+	if (!isfinite(u) || s < 0.0 || 1.0 < s) return false;
+
+	hitPosition = a + u * (b - a);
+	return true;
+}
+
+#endif // LINE_SEGMENT_HLSL
