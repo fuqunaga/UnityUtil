b99ff41afb728b9617f53fa927adac5c45613027
fuqunaga <fukhid@gmail.com>
2026-01-07 15:58:20 +0900

    feat: TextureUtil.CheckSizeAndGenerateIfNeed()

--- Packages/ga.fuquna.unityutil/Texture/TextureUtil.cs
+++ Packages/ga.fuquna.unityutil/Texture/TextureUtil.cs
@@ -5,6 +5,25 @@ namespace UnityUtil
 {
     public static class TextureUtil
     {
+        public static RenderTexture CheckSizeAndGenerateIfNeed(RenderTexture texture, Vector2Int size, int depth = 24, RenderTextureFormat format = RenderTextureFormat.Default, int mipCount = 0)
+        {
+            if (size.x <= 0 || size.y <= 0) return texture;
+            
+            if (texture != null
+                && (texture.width != size.x || texture.height != size.y))
+            {
+                Object.Destroy(texture);
+                texture = null;
+            }
+
+            if (texture == null)
+            {
+                texture = new RenderTexture(size.x, size.y, depth, format, mipCount);
+            }
+
+            return texture;
+        }
+        
         public static Texture2D LoadFromStreamingAssetsFolder(string path)
         {
             Texture2D ret = null;
