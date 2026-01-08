7f3d3bbd28d3b6af0de2141b82f316f11c99593d
fuqunaga <fukhid@gmail.com>
2026-01-08 15:33:07 +0900

    feat: CameraExtension

---
+++ Packages/ga.fuquna.unityutil/Extension/CameraExtension.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+#if USE_URP
+using UnityEngine.Rendering.Universal;
+#endif
+
+namespace UnityUtil
+{
+    public static class CameraExtension
+    {
+#if USE_URP
+        public static ScriptableRendererData GetScriptableRendererData(this Camera camera)
+        {
+            var cameraData = camera.GetUniversalAdditionalCameraData();
+            var scriptableRenderer = cameraData.scriptableRenderer;
+            
+            var pipelineAsset = UniversalRenderPipeline.asset;
+        
+            var renderers = pipelineAsset.renderers;
+            
+            var index = 0;
+            for(; index< renderers.Length; index++)
+            {
+                if (renderers[index] == scriptableRenderer)
+                {
+                    break;
+                }
+            }
+        
+            return pipelineAsset.rendererDataList[index];
+        }
+#endif
+    }
+}
