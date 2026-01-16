1b4ba2c1d0f2de2dc15a8a6308ffdeabd9d492fe
fuqunaga <fukhid@gmail.com>
2026-01-16 18:51:21 +0900

    feat: add CollectionExtensions

---
+++ Packages/ga.fuquna.unityutil/Extension/CollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnityUtil
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// 指定した述語に一致する要素をすべて削除します。
+        /// </summary>
+        public static int RemoveWhere<T>(this ICollection<T> collection, Func<T, bool> match)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (match == null) throw new ArgumentNullException(nameof(match));
+
+            using var _ = ListPool.GetAndFill(out var toRemove, collection.Where(match));
+
+            foreach (var item in toRemove)
+            {
+                collection.Remove(item);
+            }
+
+            return toRemove.Count;
+        }
+    }
+}
