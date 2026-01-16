0bfb70df5acef15a7d0f643de3c1ff0dfcafe683
fuqunaga <fukhid@gmail.com>
2026-01-16 18:51:03 +0900

    feat: add CollectionPoolExtensions

---
+++ Packages/ga.fuquna.unityutil/Extension/CollectionPoolExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Pool;
+
+namespace UnityUtil
+{
+    public static class ListPool
+    {
+        public static PooledObject<List<T>> GetAndFill<T>(out List<T> value, IEnumerable<T> initialContents)
+        {
+            var pooledObject = ListPool<T>.Get(out value);
+            if (initialContents != null)
+            {
+                value.AddRange(initialContents);
+            }
+
+            return pooledObject;
+        }
+    }
+
+    public static class HashSetPool
+    {
+        public static PooledObject<HashSet<T>> GetAndFill<T>(out HashSet<T> value, IEnumerable<T> initialContents)
+        {
+            var pooledObject = HashSetPool<T>.Get(out value);
+            if (initialContents != null)
+            {
+                foreach (var item in initialContents)
+                {
+                    value.Add(item);
+                }
+            }
+            return pooledObject;
+        }
+    }
+    
+    public static class DictionaryPool
+    {
+        public static PooledObject<Dictionary<TKey, TValue>> GetAndFill<TKey, TValue>(out Dictionary<TKey, TValue> value, IEnumerable<KeyValuePair<TKey, TValue>> initialContents)
+        {
+            var pooledObject = DictionaryPool<TKey, TValue>.Get(out value);
+            if (initialContents != null)
+            {
+                foreach (var (key, v) in initialContents)
+                {
+                    value.Add(key, v);
+                }
+            }
+            return pooledObject;
+        }
+    }
+}
